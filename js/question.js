1645c46f303e13d35ed3ae37b0909719a7d3fca6
Elyor <elyorsultonov0707@gmail.com>
2023-04-21 16:03:10 +0500

    first quiz website posted

---
+++ js/question.js
@@ -0,0 +1,62 @@
+// massiv yaratish va raqamni savollarni va javoblarni uzatish
+let questions = [
+  {
+    numb: 1,
+    question: "HTML nimani naglatadi ?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Proprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "CSS nimani naglatadi ?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet ",
+      "ColorFul Style Sheet",
+      "Compyuter Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+
+  {
+    numb: 3,
+    question: "PHP nimani naglatadi ?",
+    answer: "HyperText Preprocessor",
+    options: [
+      "HyperText Preprocessor",
+      "HyperText Programming",
+      "HyperText Preprogramming",
+      "HomeText Preprocessor",
+    ],
+  },
+
+  {
+    numb: 4,
+    question: "SQL nimani naglatadi ?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+
+  {
+    numb: 5,
+    question: "XML nimani naglatadi ?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXtensible Multiple Language",
+      "eXecutable Multi-Program Lnaguage",
+      "eXaming Multiple Language",
+    ],
+  },
+];
